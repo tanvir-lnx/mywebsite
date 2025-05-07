a2fe02b7ba3c0091140d4b8f92ff5253dd65b769
tanvir-lnx <tanvir.animation@gmail.com>
2025-05-07 06:10:33 +0600

    Initial Commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -53,7 +53,7 @@ export default function Contact() {
 
     // Create a new FormData object to send to Web3Forms API
     const form = new FormData();
-    form.append("access_key", "90f4b8af-e590-42b0-beaf-10b18f66a703"); // Replace with your Web3Forms access key
+    form.append("access_key", "6ddccf0d-b6b9-404c-8ec4-f2c42e87e93a"); // Replace with your Web3Forms access key
     form.append("name", formData.name);
     form.append("email", formData.email);
     form.append("subject", formData.subject || "New Contact Form Submission");
@@ -112,7 +112,7 @@ export default function Contact() {
                   </div>
                   <div>
                     <h3 className="font-semibold">Email</h3>
-                    <p className="text-gray-400">olovajs@gmail.com</p>
+                    <p className="text-gray-400">siddik2305101787@diu.edu.bd</p>
                   </div>
                 </div>
 
@@ -122,7 +122,8 @@ export default function Contact() {
                   </div>
                   <div>
                     <h3 className="font-semibold">Location</h3>
-                    <p className="text-gray-400">Laxmipure, Natore 6400</p>
+                    <p className="text-gray-400">Daffodil Smart City (DSC),
+                    Birulia, Savar, Dhaka-1216</p>
                   </div>
                 </div>
               </div>
